b0d39ac1e75ae8188ee6a5c8a7c5f61b131d89d8
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-05-08 16:02:40 -0300

    Jscript Practice

--- Javascript/pruebas.js
+++ Javascript/pruebas.js
@@ -10,7 +10,7 @@ if (a == b) {console.log("a = b")}
 
 if (a != b) {console.log("a != b")}
 
-console.log("*** THE END ***")
+console.log("Praise the Omnissiah!")
 
 // var nota = prompt("Nota?")
 var nota = 6
@@ -70,3 +70,39 @@ console.log(i);
 fc = ((a) => a + 100)
 
 console.log( fc(10) )
+
+const IVA = Number('21')
+const PI = new Number('3.141592')
+
+console.log(PI);
+console.log(typeof(PI))
+
+var num1 = 27
+console.log( num1**(1/3))
+
+Number.isSafeInteger(3); // true
+Number.isSafeInteger(2 ** 53); // false
+Number.isSafeInteger(2 ** 53 - 1); // true
+Number.isSafeInteger(NaN); // false
+Number.isSafeInteger(Infinity); // false
+Number.isSafeInteger("3"); // false
+Number.isSafeInteger(3.1); // false
+Number.isSafeInteger(3.0); // true
+
+var str1 = '27'
+
+console.log(num1 == str1)
+console.log(num1 === str1)
+
+// Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
+
+var frase = prompt("Ingrese una frase: ")
+var count_a = 0
+
+for (var i=0; 101; ++i){
+  console.log(frase[i])
+}
+
+  
+
+console.log(count_a)
